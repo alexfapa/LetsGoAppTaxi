af61b8186bf42803a9de8942d325d35796a14b5f
Alex Pinto <35609992+alexfapa@users.noreply.github.com>
2018-08-23 00:09:41 -0300

    Update PayPalConfig.java

--- SplashScreen/app/src/main/java/br/com/tutorialandroid/splashscreen/PayPalConfig.java
+++ SplashScreen/app/src/main/java/br/com/tutorialandroid/splashscreen/PayPalConfig.java
@@ -5,10 +5,9 @@ package br.com.tutorialandroid.splashscreen;
  */
 
 public class PayPalConfig {
-    public  static  final  String  PAYPAL_CLIENT_ID  =  "AULBUz-7LnApNp4qG_ws3knZjjwH2kquAdwii4pWrWTSFvHNonV6MqblMRxJM1v2AnWT25a64leb0R7T";
+    public  static  final  String  PAYPAL_CLIENT_ID  =  "";
 
-    //AWYs2qqnMxbzAghOsyrWXwgO3DDKckXGoEHaVZQEOz8E9TOyd9u1dyQxQB-Q5dWSvPZWcVX6sSTqQhtq
-            //"AarR83yjyOqV7o0x-d3KEHscPvfth7yTps8FjxCmS2T4HAPLr158Cb42piGS5PQIlpvOH5YOgAOnjpGr" ;
+   
 }
 
 
